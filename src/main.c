595f7d9df6ad71c38ee1b771d270e664f085abcc
Dirk Arnez <smalldirkalex@gmail.com>
2025-01-21 22:37:48 +0800

    - update src/main.c

--- src/main.c
+++ src/main.c
@@ -1,16 +1,46 @@
 #include <stdio.h>
 
-int add(int a, int b) {
-    return a + b;
+int answers[11] = {
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1
+};
+
+int hoc(int (*fptr)(int), int n) {
+    if (answers[n] > -1) {
+        printf("%d is saved = %d\n", n, answers[n]);
+        return answers[n];
+    } else {
+        answers[n] = fptr(n);
+        printf("%d is calculated = %d and saved\n", n, answers[n]);
+        return answers[n];
+    }
 }
 
-int hoc(int (*fptr)(int, int), int c, int d) {
-    return fptr(c, d) + 1;
+int factorial(int n) {
+    // Base Case:
+    if (n == 1) {
+        return 1;
+    }
+
+    // Multiplying the current N with the previous product
+    // of Ns
+    return n * hoc(&factorial, n - 1);
 }
 
-// hoc return a function of same signature as add
+
 int main() {
-    printf("%d", hoc(&add, 10, 5));
-
+    printf("factorial 5 = %d\n\n", hoc(&factorial, 5));
+    printf("factorial 6 = %d\n\n", hoc(&factorial, 6));
+    printf("factorial 4 = %d\n\n", hoc(&factorial, 4));
+    printf("factorial 10 = %d\n\n", hoc(&factorial, 10));
     return 0;
 }
